b9cd1673c12ba79e37e56894da6f6b717909706c
Adrian Kuciel <djmicros@gmail.com>
2013-03-21 19:52:25 +0100

    Initial push of empty rails library.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracknwin::Application.config.secret_token = '4d78688bb4ba5ca7262a4be7d7598613b9912ddd29d753731babd466320d414af62b8c9f35a18fc940eda85aeb06666a2b8f9974597b8e53920f2cae0c796d8f'
